df5f95bbea2c4018e8593485604e859fa4ca1652
Dev Mistry <162298392+Dev-Mistry02@users.noreply.github.com>
2024-05-10 19:51:37 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -15,25 +15,7 @@ menu.onclick = function(){
 
 // active Nav link
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('.nav__links li a');
 let logoLink = document.getElementById('logo');
-
-window.onscroll = () => {
-  let top = window.scrollY;
-  sections.forEach(sec => {
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
-
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach(link => {
-        link.classList.remove('active');
-      });
-      document.querySelector('.nav__links li a[href="#' + id + '"]').classList.add('active');
-    }
-  });
-};
 
 logoLink.addEventListener('click', () => {
   location.reload();
